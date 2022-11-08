d083df6c7ed07e7ab4d2e7691d3b165d35fbc616
Dandya <70721037+Dandya@users.noreply.github.com>
2022-11-08 15:24:24 +0300

    update tests_ECB.cpp

--- tests/tests_ECB.cpp
+++ tests/tests_ECB.cpp
@@ -51,7 +51,7 @@ TEST(ECB, TestFromGOST) {
     fread(&block, SIZE_BLOCK, 1, close_text);
     EXPECT_EQ(0x3a02c4c5aa8ada98, block.half[0]);
     EXPECT_EQ(0xd0b09ccde830b9eb, block.half[1]);
-    fread(&block, SIZE_BLOCK, 1, close_text);
+    EXPECT_EQ(SIZE_BLOCK, fread(&block, 1, SIZE_BLOCK+1, close_text));
     EXPECT_EQ(feof(close_text), 0);
     fclose(close_text);
 
@@ -116,6 +116,119 @@ TEST(ECB, PROC_ADD_NULLS_1_full) {
         EXPECT_EQ(0x7f679d90bebc2430, block.half[1]);
     }
     EXPECT_EQ(0, fread(&block, 1, SIZE_BLOCK, close_text));
+    EXPECT_NE(feof(close_text), 0);
+    fclose(close_text);
+
+    open_text = fopen("OpenText.txt", "w");
+    close_text = fopen("CloseText.txt", "r");
+    EXPECT_EQ(0, decryptECBKuz(close_text, open_text, key, PROC_ADD_NULLS_1, 0));
+    fclose(open_text);
+    fclose(close_text);
+
+    open_text = fopen("OpenText.txt", "r");
+    for(int i = 0; i < 10; i++) {
+        fread(&block, SIZE_BLOCK, 1, open_text);
+        EXPECT_EQ(0xffeeddccbbaa9988, block.half[0]);
+        EXPECT_EQ(0x1122334455667700, block.half[1]);
+    }
+    EXPECT_EQ(0, fread(&block, 1, SIZE_BLOCK, close_text));
+    EXPECT_NE(feof(open_text), 0);
+    fclose(open_text);
+}
+
+TEST(ECB, PROC_ADD_NULLS_1_not_full) {
+    FILE * open_text = fopen("OpenText.txt", "w");
+    FILE * close_text;
+    if(open_text == NULL)
+    {
+        printf("Error of open file: %d\n", __LINE__);
+        return;
+    }
+
+    vector128bit key[2];
+    key[0].half[0] = 0x0123456789abcdef;
+    key[0].half[1] = 0xfedcba9876543210;
+    key[1].half[0] = 0x0011223344556677;
+    key[1].half[1] = 0x8899aabbccddeeff;
+    
+    vector128bit block;
+    block.half[0] = 0xffeeddccbbaa9988;
+    block.half[1] = 0x1122334455667700; 
+    for(int i = 0; i < 10; i++) {
+        fwrite(&block, SIZE_BLOCK, 1, open_text);
+    }
+    fwrite(&block, 4, 1, open_text);
+    fclose(open_text);
+
+    open_text = fopen("OpenText.txt", "r");
+    close_text = fopen("CloseText.txt", "w");
+    EXPECT_EQ(0, encryptECBKuz(open_text, close_text, key, PROC_ADD_NULLS_1));
+    fclose(open_text);
+    fclose(close_text);
+
+    close_text = fopen("CloseText.txt", "r");
+    for(int i = 0; i < 10; i++) {
+        fread(&block, SIZE_BLOCK, 1, close_text);
+        EXPECT_EQ(0x5a468d42b9d4edcd, block.half[0]);
+        EXPECT_EQ(0x7f679d90bebc2430, block.half[1]);
+    }
+    EXPECT_EQ(SIZE_BLOCK, fread(&block, 1, SIZE_BLOCK+1, close_text));
+    EXPECT_NE(feof(close_text), 0);
+    fclose(close_text);
+
+    open_text = fopen("OpenText.txt", "w");
+    close_text = fopen("CloseText.txt", "r");
+    EXPECT_EQ(0, decryptECBKuz(close_text, open_text, key, PROC_ADD_NULLS_1, 4));
+    fclose(open_text);
+    fclose(close_text);
+
+    open_text = fopen("OpenText.txt", "r");
+    for(int i = 0; i < 10; i++) {
+        fread(&block, SIZE_BLOCK, 1, open_text);
+        EXPECT_EQ(0xffeeddccbbaa9988, block.half[0]);
+        EXPECT_EQ(0x1122334455667700, block.half[1]);
+    }
+    EXPECT_EQ(4, fread(&block, 1, 5, close_text));
+    EXPECT_NE(feof(open_text), 0);
+    fclose(open_text);
+}
+
+TEST(ECB, PROC_ADD_NULLS_2_full) {
+    FILE * open_text = fopen("OpenText.txt", "w");
+    FILE * close_text;
+    if(open_text == NULL)
+    {
+        printf("Error of open file: %d\n", __LINE__);
+        return;
+    }
+
+    vector128bit key[2];
+    key[0].half[0] = 0x0123456789abcdef;
+    key[0].half[1] = 0xfedcba9876543210;
+    key[1].half[0] = 0x0011223344556677;
+    key[1].half[1] = 0x8899aabbccddeeff;
+    
+    vector128bit block;
+    block.half[0] = 0xffeeddccbbaa9988;
+    block.half[1] = 0x1122334455667700; 
+    for(int i = 0; i < 10; i++) {
+        fwrite(&block, SIZE_BLOCK, 1, open_text);
+    }
+    fclose(open_text);
+
+    open_text = fopen("OpenText.txt", "r");
+    close_text = fopen("CloseText.txt", "w");
+    EXPECT_EQ(0, encryptECBKuz(open_text, close_text, key, PROC_ADD_NULLS_1));
+    fclose(open_text);
+    fclose(close_text);
+
+    close_text = fopen("CloseText.txt", "r");
+    for(int i = 0; i < 10; i++) {
+        fread(&block, SIZE_BLOCK, 1, close_text);
+        EXPECT_EQ(0x5a468d42b9d4edcd, block.half[0]);
+        EXPECT_EQ(0x7f679d90bebc2430, block.half[1]);
+    }
+    EXPECT_EQ(SIZE_BLOCK, fread(&block, 1, SIZE_BLOCK+1, close_text));
     EXPECT_NE(feof(close_text), 0);
     fclose(close_text);
 
