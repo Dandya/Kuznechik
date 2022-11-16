a367ee600abe47e447ae88862ea63c5f5a9d5a50
Dandya <Aleksandrmen6@gmail.com>
2022-11-16 17:46:47 +0300

    Upgrade main

--- src/main.c
+++ src/main.c
@@ -6,257 +6,184 @@
 #include "../include/kuznechik.h"
 #include "../include/sha256.h"
 
+struct input_names {
+    char* input_name = NULL;
+    struct input_names* next = NULL;
+    struct input_names* prev = NULL;
+}
+
 int parameterDefinition(char* prm);
 int strEQ(char* strFirst,char * strSecond);
 int modeDefinition(char* mode);
+int readInputNames(char** argv, int argc, int index, struct input_names * root);
 int isRegularFile(char *path);
 int isDirectory(char *path);
 
-
-uint64_t main(int argc, char** argv)
-/*parameters: 
-*  -i names of input files or directores #1
-*  -k string with key #2
-*  -m mode of a encryption or decryption {"ECB", "CBC", "CTR", "OFB", "CFB", "IMITO"} #3
-*  -p mode of padding block {1-3} #4
-*  -c encrypt or decrypt {e, d} #5
-*  -b count byte in last block for first mode padding block {1-8} #6
-*  -s size MAC in bits {1-128} #7
-*  -h help #8
-*/
-{
-    // standard parameters
-    char* name_input = NULL;
-    char* name_output =  NULL;
+/// @brief 
+/// parameters: 
+/// *  -i names of input files or directores #1
+/// *  -k string with key #2
+/// *  -m mode of a encryption or decryption {"ECB", "CBC", "CTR", "OFB", "CFB", "IMITO"} #3
+/// *  -p mode of padding block {2 or 3} #4
+/// *  -c encrypt or decrypt {e, d} #5
+/// *  -s size MAC in bits {1-128} #6
+/// *  -h help #7
+/// Standart parameters: 
+/// *    encryption by mode "ECB" with second mode of padding block. 
+/// *    Size MAC for mode "IMITO" is 64 bits. 
+/// @param argc 
+/// @param argv 
+/// @return 0 is good, 1 is error of malloc, 2 is error in parameters;
+int main(int argc, char** argv) {
+    struct input_names list_names;
+    char* key_str = NULL;
     int mode = 'e';
     int mode_of_encryption = ECB;
     int mode_padding = PROC_ADD_NULLS_2;
-    uint8_t countByteInLastBlock = 0; 
-    uint8_t sizeMAC  = 64; 
-    // read input parameters
+    uint8_t sizeMAC  = 64;  
+
     for(int index = 1; index < argc; index++)
     {
         switch(parameterDefinition(argv[index]))
         {
-            case 1: 
+            case 1: { // -i
                 index++;
-                if(index == argc)
-                {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
+                if(index == argc) {
+                    fprintf(stderr, "Fatal error: empty parameter -p\n");
+                    return 2;
                 }
-                nameInputFile = argv[index];
-                break;
-            case 2: 
-                index++;
-                if(index == argc)
-                {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
-                }
-                nameFileWithKey = argv[index];
-                break;
-            case 3: 
-                index++;
-                if(index == argc)
-                {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
-                }
-                if((mode = modeDefinition(argv[index])) == 0)
-                {
-                    printf("Fatal error: unknown mode\n");
-                    return 0;
-                }
-                if(mode == 6 || mode == 1)
-                {
-                    break;    
-                }
-                else
-                {
-                    printf("Fatal error: invalid mode\n");
-                    return 0;
-                }
-            case 4: 
-                index++;
-                if(index == argc)
-                {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
-                }
-                modePadding = *(argv[index]) - '1' + 1;
-                if(modePadding >= 1 && modePadding <=3)
-                {
-                    break;
-                }
-                else
-                {
-                    printf("Fatal error: invalid modePadding\n");
-                    return 0;
-                }
-            case 6: 
-                index++;
-                if(index == argc)
-                {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
-                }
-                crypt = *(argv[index]);
-                if(crypt != 'e' && crypt != 'd')
-                {
-                    printf("Fatal error: invalid mode crypt\n");
-                    return 0;
+                index = readInputNames(argv, argc, index, &list_names);
+                if(parameterDefinition(list_names.input_name) != -1) {
+                    fprintf(stderr, "Fatal error: empty parameter -p\n");
+                    return 2;
                 }
                 break;
-             case 6: 
+            }
+            case 2: { // -k
+                index++;
+                if(index == argc) {
+                    fprintf(stderr, "Fatal error: empty parameter -k\n");
+                    return 2;
+                }
+                key_str = argv[index];
+                if(parameterDefinition(key_str) != -1) {
+                    fprintf(stderr, "Fatal error: empty parameter -k\n");
+                    return 2;
+                }
+                break;
+            }
+            case 3: { // -m
                 index++;
                 if(index == argc)
                 {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
+                    fprintf(stderr, "Fatal error: empty parameter -m\n");
+                    return 2;
                 }
-                countByteInLastBlock = *(argv[index]) - '1' + 1;
-                if(countByteInLastBlock <= 8 && countByteInLastBlock >= 1)
+                mode_of_encryption = modeDefinition(argv[index]);
+                if(mode_of_encryption == 0)
                 {
-                    break;    
+                    fprintf(stderr, "Fatal error: unknown mode in parameter -m\n");
+                    return 2;
                 }
-                else
-                {
-                    printf("Fatal error: invalid countByteInLastBlock\n");
-                    return 0;
-                }
-            case 7: 
+            }
+            case 4: { // -p
                 index++;
                 if(index == argc)
                 {
-                    printf("Fatal error: empty parameter\n");
-                    return 0;
+                    fprintf(stderr, "Fatal error: empty parameter -p\n");
+                    return 2;
+                }
+                modePadding = *(argv[index]) - '0';
+                if(modePadding < 1 ||  modePadding > 3)
+                {
+                    fprintf(stderr, "Fatal error: invalid mode of padding block\n");
+                    return 2;
+                }
+            }
+            case 5: { // -c
+                index++;
+                if(index == argc)
+                {
+                    fprintf(stderr, "Fatal error: empty parameter -c\n");
+                    return 2;
+                }
+                mode = *(argv[index]);
+                if(mode != 'e' && mode != 'd')
+                {
+                    fprintf(stderr, "Fatal error: invalid mode encrypt or decrypt\n");
+                    return 2;
+                }
+                break;
+            }
+            case 6: { // -s
+                index++;
+                if(index == argc)
+                {
+                    fprintf(stderr, "Fatal error: empty parameter -s\n");
+                    return 2;
                 }
                 sizeMAC = (uint8_t)atoi(argv[index]);
-                if(sizeMAC <= 64 && sizeMAC >= 1)
+                if(sizeMAC > 128 || sizeMAC < 1)
                 {
-                    break;    
+                    fprintf(stderr, "Fatal error: invalid size MAC\n");
+                    return 2;
                 }
-                else
-                {
-                    printf("Fatal error: invalid sizeMAC\n");
-                    return 0;
-                }
-            case 8:
-                printf("Parameters:\n*  -i name input file\n* -k string with key\n*  -m mode {ECB, CBC, CTR, OFB, CFB, IMITO}\n*  -p mode of padding block {1-3}\n*  -c encrypt or decrypt {e, d}\n*  -b count byte in last block for first mode of padding block {1-8}\n*  -s size MAC {1-128}\n*  -h help\n");
-                return 1;
-            case -1: 
-                printf("Fatal error: invalid parameter\n");
+            }
+            case 7: { // -h
+                fprintf(stderr, "Parameters:\n*  -i name input file\n* -k string with key\n*  -m mode {ECB, CBC, CTR, OFB, CFB, IMITO}\n*  -p mode of padding block {1-3}\n*  -c encrypt or decrypt {e, d}\n*  -b count byte in last block for first mode of padding block {1-8}\n*  -s size MAC {1-128}\n*  -h help\n");
                 return 0;
+            }
+            case -1: {
+                fprintf(stderr, "Fatal error: invalid parameter\n");
+                return 2;
+            }
         }
     }
-    //check
-    if(nameInputFile == NULL && nameFileWithKey == NULL)
+
+    if(list_names.input_name == NULL ||  key == NULL)
     {
-        printf("Fatal error: insufficient parameters\n");
-        return 0;
+        fprintf(stderr, "Fatal error: insufficient parameters\n");
+        return 2;
     }
-    //read key
-    uint32_t key[8];
-    FILE* fileWithKey = fopen(nameFileWithKey, "r");
-    if(fileWithKey == NULL)
-    {
-        printf("Fatal error: could not open file with key\n");
-        return 0;
+    
+    vector128_t key_vec[2];
+    int result = sha256((uint8_t *)key, strlen(key), key_vec);
+    if(result < 0) { 
+        fprintf(stderr, "Fatal error: error of getting hash of key\n Result: %d\n", result);
     }
-    if(fread(key, 4, 8, fileWithKey) != 8)
-    {
-        printf("Fatal error: could not read file with key\n");
-        return 0;
+    vector128_t iteration_keys[10];
+    result = createIterationKeysKuz(key_vec, iteration_keys);
+    if(result < 0) { 
+        fprintf(stderr, "Fatal error: error of creating of iteration keys\n Result: %d\n", result);
     }
-    fclose(fileWithKey);
-    //begin
-    switch(mode)
-    {
-        case ECB:
-           if(crypt == 'e')
-           {
-               if(EncryptECB(nameInputFile, nameOutputFile, key, modePadding) != 1)
-               {
-                    printf("Fatal error: failed to complete EncryptECB");
-                    return 0;
-               }
-           }
-           else
-           {
-                if(DecryptECB(nameInputFile, nameOutputFile, key, modePadding, countByteInLastBlock) != 1)
-               {
-                    printf("Fatal error: failed to complete DecryptECB");
-                    return 0;
-               }
-           }
-           break;
-        case IMITO:
-           {
-           uint64_t result = getMAC(nameInputFile, key, sizeMAC);
-           printf("%lld\n", result);
-           return result;
-           }
-    }
-    return 1;
+
+
+    
+    return 0;
 }
 
 int parameterDefinition(char* prm)
 {
-    if(*prm != '-')
+    if(prm[0] != '-' || prm[2] != 0)
     {
         return -1;
     }
     switch(prm[1])
     {
         case 'i': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
             return 1;
         case 'k': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
             return 2;
         case 'm': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
             return 3;
         case 'p': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
             return 4;
         case 'c': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
             return 5;
-        case 'b': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
-            return 6;
         case 's': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
-            return 7;
+            return 6;
         case 'h': 
-            if(prm[2] != 0)
-            {
-                return -1;
-            }
-            return 8;
+            return 7;
         default: return -1;
     }
 }
@@ -295,6 +222,25 @@ int modeDefinition(char* mode) {
         return IMITO;
     }
     return 0;
+}
+
+int readInputNames(char ** argv, int argc, int index, struct input_names * root) {
+    struct input_names * node = root;
+    while( index < argc ) { 
+        if(parameterDefinition(argv[index]) != -1) {
+            return index-1;
+        }
+        node->input_name = argv[index];
+        node->next = (struct input_names *)malloc(sizeof(struct input_names));
+        if(node->next == NULL) { 
+            fprintf(stderr, "Fatal error: error of malloc\n Line: %d\n", __LINE__);
+            exit(1);
+        }
+        node->next->prev = node;
+        node = node->next;
+        index++;
+    }
+    return index;
 }
 
 int isRegularFile(char *path) {
